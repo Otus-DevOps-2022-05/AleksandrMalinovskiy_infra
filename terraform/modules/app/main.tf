80c50964f6e5fffaba1d6c95c3a29b4403f5512d
alexM <https://gitlab.loldev.ru>
2022-07-22 09:51:52 +0300

    DZ Terraform 2

---
+++ terraform/modules/app/main.tf
@@ -0,0 +1,28 @@
+resource "yandex_compute_instance" "app" {
+  name = "reddit-app"
+
+  labels = {
+    tags = "reddit-app"
+  }
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      # Указать id образа созданного в предыдущем домашем задании
+      image_id = var.app_disk_image
+    }
+  }
+
+  network_interface {
+    # Указан id подсети default-ru-central1-a
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+}
