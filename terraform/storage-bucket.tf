fde2235072fda40761bafcfb6264f77120eb2083
alexM <https://gitlab.loldev.ru>
2022-07-22 12:35:05 +0300

    DZ Terraform 2

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,5 +1,5 @@
 provider "yandex" {
-  service_account_key_file = var.account_key_path
+  service_account_key_file = var.service_account_key_file
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
   zone                     = var.zone
